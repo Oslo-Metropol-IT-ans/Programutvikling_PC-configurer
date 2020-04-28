c087fde307ebf28b7ed529602f5603e6d43e2166
Nicolai Molstad <s341833@oslomet.no>
2020-04-28 12:04:40 +0200

    login

--- src/main/java/Dataamatorene/Controllers/LogInController.java
+++ src/main/java/Dataamatorene/Controllers/LogInController.java
@@ -27,6 +27,7 @@ public class LogInController {
 
         BrukerRegister.setAktivBruker(null);
 
+        // Laster inn brukere
         FileOpener opener = new FileOpenerJobj();
         try {
             BrukerRegister.setBrukere((ArrayList<Bruker>) opener.read("src/main/java/Dataamatorene/Files/Login.jobj"));
@@ -36,6 +37,7 @@ public class LogInController {
             e.printStackTrace();
         }
 
+        // Setter metode på entertrykk i tetfield
         txtBrukernavn.setOnKeyPressed(keyEvent -> {
             if(keyEvent.getCode() == KeyCode.ENTER){
                 txtPassword.requestFocus();
@@ -51,6 +53,7 @@ public class LogInController {
 
     }
 
+    // FXML deklarering
     @FXML
     private TextField txtBrukernavn;
 
@@ -66,22 +69,27 @@ public class LogInController {
     @FXML
     private Label lblTilbakemeling;
 
+    // Metode for å logge inn
     @FXML
     void loggInn(Event event) {
 
+        // Henter alle brukere
         List<Bruker> liste = BrukerRegister.getBrukere();
 
+        // Registrerer input
         String brukernavn = txtBrukernavn.getText();
         String passord = txtPassword.getText();
 
         boolean finnes = false;
 
+        // Kjører løkke for å se etter match med innput
         for(Bruker b:liste){
             if (brukernavn.equalsIgnoreCase(b.getBrukernavn()) && passord.equals(b.getPassord())){
                 finnes = true;
                 BrukerRegister.setAktivBruker(b);
                 if (b.isSuperbruker()){
                     try {
+                        // Går til meny admin ved funn av superbruker
                         App.setRoot("menyadmin");
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -89,7 +97,7 @@ public class LogInController {
                 }
                 else{
                     try {
-
+                        // Går til meny bruker ved funn av sluttbruker
                         App.setRoot("menybruker");
                     } catch (IOException e) {
                         Dialogs.showErrorDialog("Denne siden finnes ikke");
@@ -98,6 +106,7 @@ public class LogInController {
             }
         }
 
+        // Tilbakemelding til bruker ved misslykket innlogging
         if(!finnes){
             Dialogs.showErrorDialog("Brukernavn eller passord er feil");
             txtPassword.setText("");
@@ -106,6 +115,7 @@ public class LogInController {
 
     }
 
+    // Knapp til registrering av ny bruker
     @FXML
     void nyBruker(ActionEvent event) {
         try {

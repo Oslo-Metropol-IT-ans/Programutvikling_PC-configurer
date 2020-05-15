98b1c9d0e44309a2c3a5f644f069ba9b50df3066
Nicolai Molstad <s341833@oslomet.no>
2020-05-15 10:25:09 +0200

    Update ThreadSaverJobj.java

--- src/main/java/Dataamatorene/Filbehandling/ThreadSaverJobj.java
+++ src/main/java/Dataamatorene/Filbehandling/ThreadSaverJobj.java
@@ -3,27 +3,24 @@ package Dataamatorene.Filbehandling;
 import Dataamatorene.Tasks.ThreadSaveJobj;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.scene.layout.Pane;
-import org.w3c.dom.Node;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ThreadSaverJobj {
 
-    private static ThreadSaveJobj threadSaveJobj;
-
     private ArrayList<? extends Serializable> liste;
     private String path;
-    private Pane node;
+    private Pane pane;
 
-    public ThreadSaverJobj(ArrayList<? extends Serializable> liste, String path, Pane node) {
+    public ThreadSaverJobj(ArrayList<? extends Serializable> liste, String path, Pane pane) {
         this.path = path;
         this.liste = liste;
-        this.node = node;
+        this.pane = pane;
     }
 
     public void save() {
-        threadSaveJobj = new ThreadSaveJobj(liste, path);
+        ThreadSaveJobj threadSaveJobj = new ThreadSaveJobj(liste, path);
         threadSaveJobj.setOnSucceeded(this::threadSaveJobjSuccess);
         threadSaveJobj.setOnRunning(this::threadSaveJobjRunning);
         Thread th = new Thread(threadSaveJobj);
@@ -32,10 +29,10 @@ public class ThreadSaverJobj {
     }
 
     private void threadSaveJobjSuccess (WorkerStateEvent e) {
-        node.setDisable(false);
+        pane.setDisable(false);
     }
 
     private void threadSaveJobjRunning (WorkerStateEvent e) {
-        node.setDisable(true);
+        pane.setDisable(true);
     }
 }
